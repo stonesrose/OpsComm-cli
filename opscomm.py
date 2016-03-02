821cc70e1b21cad29c3828511316a3a05dbb0a78
Michael Luich <mluich@stonesrose.com>
2016-03-02 20:42:03 +0000

    200160302-15:42 MJL added license file. 200160302-15:42 MJL added opscomm-cli.conf.template 200160302-15:42 MJL opscomm-cli now looks for and parses the  config file.

--- opscomm.py
+++ opscomm.py
@@ -11,23 +11,30 @@ Usage:
 Options:
   -h --help                              Show this screen.
   --version                              Show version.
-  -s SERVER --server=SERVER              Set Server [default: opscomm-server-core-mluich.c9users.io]
+  -s SERVER --server=SERVER              Set Server Address
   -p PORT --port=PORT                    Set Port [default: 8080]
   -F --FULL                              Use Full detail listing
   -c COLUMN --columns=COLUMNS            Set Columns [default: 25]
   -L LEVEL --logging=level               Set Debug Level critical, error, warning, info, debug  [default: error]
 
 Examples:
-  opscomm devices n3b01
+  opscomm search n3b01
 
 Help:
   Yeah we all need some help.
 
 """
+
+#[default: opscomm-server-core-mluich.c9users.io]
+import os
 from inspect import getmembers, isclass, ismodule
 from docopt import docopt
 import logging
 import pkg_resources  # part of setuptools
+import ConfigParser
+
+
+
 #version = pkg_resources.require("opscomm")[0].version
 version = "0.0.20160228"
 logger = logging.getLogger()
@@ -41,13 +48,37 @@ logger.addHandler(handler)
 def main():
     """Main opscomm entrypoint."""
     import datastores
+    options={}
+    # set an initial logging level
+    logger.setLevel(logging.DEBUG)
+    
+    
+    #Since command line options overide config file options 
+    #Grab the config files first!
+    Config = ConfigParser.ConfigParser()
+    try:
+     Config.readfp(open('/etc/opscomm/opscomm-cli.conf'))
+    except:
+      logging.warning("/etc/opscomm/opscomm-cli.conf NOT FOUND")
+      
+    Config.read(['site.cfg', os.path.expanduser('~/.opscomm/opscomm-cli.conf')])
+    
+    
+    for section_name in Config.sections():
+      if section_name != 'server': continue
+      logging.debug('Section: %s' % section_name)
+      logging.debug('  Options: %s'% Config.options(section_name))
+      for name, value in Config.items(section_name):
+        logging.debug( '  %s = %s' % (name, value))
+        options[name]=value
 
-
-
-    options = docopt(__doc__, version=version)
-    for key in options.keys():
+    #And Commandline options overwrite
+    docOptions = docopt(__doc__, version=version)
+    for key in docOptions.keys():
         #print key, options[key]
-        options[key.strip('-')] = options[key]
+        cleanKey=key.strip('-')
+        if cleanKey in options: continue
+        options[cleanKey] = docOptions[key]
 
     #Set logging level:
     if options['logging'] == "critical": logger.setLevel(logging.CRITICAL)
@@ -59,7 +90,7 @@ def main():
 
     logging.debug("options: ")
     logging.debug(options)
-    #print (options['logging'])
+    print (options['logging'])
 
     for name, data in getmembers(datastores, isclass):
       if name == '__builtins__': continue
